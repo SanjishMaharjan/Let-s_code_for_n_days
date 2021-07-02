31f5be86158e15259ccffe151baee9d1986c153e
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2021-07-02 17:04:39 +0545

    upon adding time gets its actual format

--- Day2/time.cpp
+++ Day2/time.cpp
@@ -3,34 +3,41 @@
 //2. Void sum(time,time)- to add 2 different time
 //3. void display()- to display the the resultant time
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Time{
-    public :
-    int hours,min ,second;
-     
-    void getTime(){
-        cout<<"Enter time in hours, min, second "<<endl;
-        cin>>hours>>min>>second;
+class Time
+{
+public:
+    int hours, min, second;
+
+    void getTime()
+    {
+        cout << "Enter time in hours, min, second " << endl;
+        cin >> hours >> min >> second;
     }
-    Time sum(Time time1,Time  time2){
-            Time sum;
-            sum.hours=time1.hours+time2.hours;
-            sum.min=time1.min+time2.min;
-            sum.second=time1.second+time2.second;
-            return sum;
+    void sum(Time time1, Time time2)
+    {
+
+        second = time1.second + time2.second;
+        min = time1.min + time2.min + (second / 60);
+        hours = time1.hours + time2.hours + (min / 60);
+        second %= 60;
+        min %= 60;
     }
-    void display(){
-        cout<<"hour="<<hours<<endl<<"minutes="<<min<<endl<<"second="<<second<<endl;
+    void display()
+    {
+        cout << "Hour=" << hours << endl
+             << "Minutes=" << min << endl
+             << "Second=" << second << endl;
     }
-}; 
+};
 int main()
 {
-   Time T1,T2,T3,T4;
-   T1.getTime();
-   T2.getTime();
-   T3=T3.sum(T1,T2);
-   T3.display();
-return 0;
+    Time T1, T2, T3;
+    T1.getTime();
+    T2.getTime();
+    T3.sum(T1, T2);
+    T3.display();
+    return 0;
 }
